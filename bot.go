554db1d962cccc7f9d375cd48aaab1e50d6e20d1
aikchun <iam@simaikchun.com>
2023-03-30 06:48:27 +0800

    totobro-bot-15: fetchNextDraw now broadcasts next draw if the draw is today.

--- bot.go
+++ bot.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/aikchun/gotelegrambot"
 	"github.com/aikchun/gototo"
@@ -74,11 +75,13 @@ func (b Bot) nextDraw(u *gotelegrambot.Update, args []string) {
 }
 
 func nextDrawMessage(n gototo.NextDraw) string {
-	t := `Next Toto Draw
-	
-	Date: %s
-	Prize: %s`
-	return fmt.Sprintf(t, n.GetDate(), n.GetPrize())
+	isToday := isNextDrawToday(n)
+
+	if isToday {
+		return fmt.Sprintf("The draw is today!\n\nDate: %s\nPrize: %s", n.GetDate(), n.GetPrize())
+	}
+
+	return fmt.Sprintf("Next Toto Draw\n\nDate: %s\nPrize: %s", n.GetDate(), n.GetPrize())
 }
 
 func (b Bot) results(u *gotelegrambot.Update, args []string) {
@@ -171,7 +174,6 @@ func (b Bot) unsubscribe(u *gotelegrambot.Update, args []string) {
 	if err := b.subscriptionService.SoftDelete(chatID); err != nil {
 		log.Println(err)
 		p.Text = "unsubscribe unsuccessful"
-
 	}
 
 	if _, err := gotelegrambot.SendMessage(b.token, p); err != nil {
@@ -181,7 +183,49 @@ func (b Bot) unsubscribe(u *gotelegrambot.Update, args []string) {
 }
 
 func (b Bot) fetchNextDraw(u *gotelegrambot.Update, args []string) {
+	for _, p := range b.createNextDrawBroadcastPayloads(u, gototo.GetNextDraw()) {
+		if _, err := gotelegrambot.SendMessage(b.token, p); err != nil {
+			log.Fatalln(err)
+		}
+	}
 
+}
+
+func (b Bot) createNextDrawBroadcastPayloads(u *gotelegrambot.Update, n gototo.NextDraw) []gotelegrambot.SendMessagePayload {
+
+	if !isValidFetchNextDrawUpdate(u) {
+		log.Fatalln("Not valid payload")
+	}
+
+	subs, err := b.subscriptionService.GetAll()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	_, err = b.nextDrawService.Save(n)
+
+	isToday := isNextDrawToday(n)
+
+	if err != nil && !isToday {
+		log.Fatalln("failed to save next draw and next draw is not today")
+	}
+
+	var ps []gotelegrambot.SendMessagePayload
+
+	for _, s := range subs {
+		p := gotelegrambot.SendMessagePayload{
+			ChatID: s.ChatID,
+			Text:   nextDrawMessage(n),
+		}
+		ps = append(ps, p)
+	}
+
+	return ps
+
+}
+
+func isValidFetchNextDrawUpdate(u *gotelegrambot.Update) bool {
 	id, err := strconv.ParseInt(os.Getenv("FETCH_NEXT_DRAW_TASK_UPDATE_ID"), 10, 64)
 	if err != nil {
 		log.Fatal(err)
@@ -200,34 +244,27 @@ func (b Bot) fetchNextDraw(u *gotelegrambot.Update, args []string) {
 		log.Fatal(err)
 	}
 
-	if u.UpdateID == id && u.Message.MessageID == mid && u.Message.From.ID == uid && u.Message.From.FirstName == os.Getenv("FETCH_NEXT_DRAW_TASK_USER_FIRST_NAME") && u.Message.Chat.ID == cid {
-		n := gototo.GetNextDraw()
+	return u.UpdateID == id &&
+		u.Message.MessageID == mid &&
+		u.Message.From.ID == uid &&
+		u.Message.From.FirstName == os.Getenv("FETCH_NEXT_DRAW_TASK_USER_FIRST_NAME") && u.Message.Chat.ID == cid
+}
 
-		_, err := b.nextDrawService.Save(n)
+func isNextDrawToday(n gototo.NextDraw) bool {
+	now := time.Now().Add(time.Hour * 8)
+	nowString := now.Format("Mon, 02 Jan 2006")
 
-		if err != nil {
-			log.Fatal(err)
-		}
+	return nowString == parseNextDrawDateString(n.GetDate())
+}
 
-		if err == nil {
-			subs, err := b.subscriptionService.GetAll()
+func parseNextDrawDateString(dateString string) string {
+	s := strings.Split(dateString, " ")
 
-			if err != nil {
-				log.Fatal(err)
-			}
-			ndm := nextDrawMessage(n)
+	var ds []string
 
-			for _, s := range subs {
-				p := gotelegrambot.SendMessagePayload{
-					ChatID: s.ChatID,
-					Text:   ndm,
-				}
-				if _, err := gotelegrambot.SendMessage(b.token, p); err != nil {
-					log.Fatal(err)
-				}
-			}
-		}
-
+	for i := 0; i < 4; i++ {
+		ds = append(ds, s[i])
 	}
 
+	return strings.Join(ds, " ")
 }
